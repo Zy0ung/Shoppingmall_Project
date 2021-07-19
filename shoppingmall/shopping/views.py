b978cfdcc9d4cd7303e39797d2555772091205dd
Jiyoung <thdwldud8@gmail.com>
2021-07-20 01:02:51 +0900

    base.html 생성

--- shoppingmall/shopping/views.py
+++ shoppingmall/shopping/views.py
@@ -11,35 +11,35 @@ def home(request):
 def detail(request):
     return render(request, 'detail.html')
 
-def new(request):
-    return render(request, 'new.html')
-
 def create(request):
-    new_item = Item()
-    new_item.name = request.POST['name']
-    new_item.price = request.POST['price']
-    new_item.image = request.FILES['image']
-    new_item.body = request.POST['body']
-    new_item.save() 
-    return redirect('home')
+    if request.method == 'POST':
+        new_item = Item()
+        new_item.name = request.POST['name']
+        new_item.price = request.POST['price']
+        new_item.image = request.FILES['image']
+        new_item.body = request.POST['body']
+        new_item.save() 
+        return redirect('home')
+    else:
+        return render(request, 'new.html')
 
 def detail(request, id):
     item = get_object_or_404(Item, pk=id)
     return render(request, 'detail.html', {'item' : item})
 
-def edit(request, id):
-    item = Item.objects.get(id = id)
-    return render(request, 'edit.html', {'item':item})
-
 def update(request, id):
-    update_item = Item.objects.get(id = id)
-    update_item.name = request.POST['name']
-    update_item.price = request.POST['price']
-    update_item.image = request.FILES['image']
-    update_item.body = request.POST['body']
-    update_item.save()
-    return redirect('detail', update_item.id)
-
+    if request.method == 'POST':
+        update_item = Item.objects.get(id = id)
+        update_item.name = request.POST['name']
+        update_item.price = request.POST['price']
+        update_item.image = request.FILES['image']
+        update_item.body = request.POST['body']
+        update_item.save()
+        return redirect('detail', update_item.id)
+    else:
+        item = Item.objects.get(id = id)
+        return render(request, 'edit.html', {'item':item})
+        
 def delete(request, id):
     delete_item = Item.objects.get(id = id)
     delete_item.delete()

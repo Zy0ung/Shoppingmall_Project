47bb2a3494c50ff1e1a0ab2479c71f37d6ea4e96
Jiyoung <thdwldud8@gmail.com>
2021-07-20 19:00:11 +0900

    url 수정

--- shoppingmall/account/urls.py
+++ shoppingmall/account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('account/signup', views.signup, name='signup'),
     path('account/user_login', views.user_login, name='user_login'),
     path('account/user_logout', views.user_logout, name='user_logout'),
     path('account/mypage', views.mypage, name="mypage"),
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
